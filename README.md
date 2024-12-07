fb44b4511f90b08d2fe09a080d806f4532966545
Gregory Vincic <gregoryvse@gmail.com>
2024-12-07 16:52:30 +0100

    Switch documentation

--- README.md
+++ README.md
@@ -2,15 +2,8 @@
 
 ![](security_detail.svg)
 
-In the oauth2 flow the state is
-
-  GUARDNAME.RANDOM.SIGNATURE.DESTINATION
-
-The random value and signature are verified using the security detail
-private key.
-
-The destination part can be used by you as a way to redirect users to
-whatever they wanted to get in the first place.
+The security detail, with it's guards, protect your handlers.
+In the oauth2 flow the state parameter is signed and verified.
 
 
 ## Quick start
--- doc.go
+++ doc.go
@@ -1,7 +1,14 @@
 /*
 Package htsec provides security detail for your endpoints.
 
-The security detail, with it's guards, protect your handlers.
-In the oauth2 flow the state parameter is signed and verified.
+In the oauth2 flow the state is
+
+	GUARDNAME.RANDOM.SIGNATURE.DESTINATION
+
+The random value and signature are verified using the security detail
+private key.
+
+The destination part can be used by you as a way to redirect users to
+whatever they wanted to get in the first place.
 */
 package htsec
