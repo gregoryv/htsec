76f5df2a12f750857781aa354fa83d2861c28323
Gregory Vincic <gregoryvse@gmail.com>
2024-12-07 16:56:12 +0100

    Update

--- changelog.md
+++ changelog.md
@@ -3,8 +3,9 @@
 This project adheres to semantic versioning and all major changes will
 be noted in this file.
 
-## [0.1.1-dev]
+## [0.2.0] 2024-12-07
 
+- Remove context from Authorize method, using context from request
 - Document types and methods
 
 ## [0.1.0] 2024-12-01
--- doc.go
+++ doc.go
@@ -1,12 +1,16 @@
 /*
 Package htsec provides security detail for your endpoints.
 
+The security detail with it's guards is used to authorize requests.
+Once authorized a slip with e.g. name, email and toke is returned for
+further use.
+
 In the oauth2 flow the state is
 
 	GUARDNAME.RANDOM.SIGNATURE.DESTINATION
 
-The random value and signature are verified using the security detail
-private key.
+The random value, signature and security detail private key are used
+to verify the state.
 
 The destination part can be used by you as a way to redirect users to
 whatever they wanted to get in the first place.
